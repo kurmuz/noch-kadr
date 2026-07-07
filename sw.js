e953c54b8fffb6eead70c5750937cf7fbd6051be
kurmuz <kurmuz@users.noreply.github.com>
2026-07-07 14:57:37 +0300

    bump cache v1->v2, refresh version params to bust cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nochkadr-v1';
+const CACHE = 'nochkadr-v2';
 const FILES = ['index.html', 'cities.js', 'azkar.js', 'adhan.js', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
@@ -12,7 +12,7 @@ self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keys) => Promise.all(
             keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
-        ))
+        )).then(() => self.clients.claim())
     );
 });
 
